96c3152b1c4610cefe4db26f8c87826faacda7fa
Draga2013 <draga_sorin@yahoo.com>
2021-01-24 11:07:29 +0200

    Task3:Fizz Buzz

--- src/main/java/Fundamentals/TasksExercises.java
+++ src/main/java/Fundamentals/TasksExercises.java
@@ -28,13 +28,35 @@ public class TasksExercises {
         float bmi = weight / ((float) height * (float) height / 10000);
         if ((bmi > 18.5f) && (bmi <= 24.9)) {
             System.out.println(bmi + "Bmi  optimal");
-        }else{
+        } else {
             System.out.println(bmi + "Bmi not optimal");
         }
     }
+
+    //    Write an application that takes a positive number from the user (type int) and writes all numbers from 1 to the given number, each on the next line, with the following changes:
+//            ● in place of numbers divisible by 3, instead of a number the program should print "Fizz"
+//            ● in place of numbers divisible by 7, instead of a number the program should write "Buzz"
+//            ● if the number is divisible by both 3 and 7, the program should print "Fizz buzz"
+//    ositive number from the user (type int) and prints all prime numbers greater than 1 and less than the given number.
+    public static void fizzbuzz() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti numar");
+        int numar = scanner.nextInt();
+        for (int i = 1; i <= numar; i++) {
+            if ((i % 3) == 0 && (i % 7) == 0) {
+                System.out.println("fizz buzz");
+
+            } else if ((i % 7) == 0) {
+                System.out.println("buzz");
+            } else if ((i % 3) == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
-
-
 
 
 
